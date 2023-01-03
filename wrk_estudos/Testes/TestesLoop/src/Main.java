5aad4da167a83330d2adafc5f60a1fc1e9158663
Leo Trevisol <104337845+Leo-Trevisol@users.noreply.github.com>
2023-01-03 16:13:03 -0300

    testes

---
+++ wrk_estudos/Testes/TestesLoop/src/Main.java
@@ -0,0 +1,49 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+	int y = 20;
+	
+//		for (int i = 0; i < y; i++) {
+//			if (i % 19 == 0) {
+//			System.out.println("Achei um número divisível por 19 entre x e y");
+//			System.out.println(i);
+//			continue;
+//			}
+//	}
+		
+//		for (int i = 0; i < 100; i++) {
+//			if (i > 50 && i < 60) {
+//			continue;
+//			}
+//			System.out.println(i);
+//			}
+		
+//		for(int i = 0; i <300; i++) {
+//			if(i<150) {
+//				continue;
+//			}
+//			System.out.println(i);
+//		}
+//		
+//		int soma = 0;
+//		int i = 1;
+//		while (i < 1000) {
+//			soma += i;
+//			i++;
+//			System.out.println(soma);
+//		}
+	
+	for(int i =0; i <100;i++) {
+		if(i % 3 == 0) {
+			System.out.println(i);
+		}
+	}
+
+}
+	
+	
+	
+}
