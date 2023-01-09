1199a14ebc29da46fcd1da6d559b67fea39727b1
Leo Trevisol <104337845+Leo-Trevisol@users.noreply.github.com>
2023-01-09 13:52:32 -0300

    att

--- wrk_estudos/Aula5-ObjetosCompostos/src/Luta.java
+++ wrk_estudos/Aula5-ObjetosCompostos/src/Luta.java
@@ -27,6 +27,15 @@ public class Luta {
 		if(aprovada) {
 			desafiado.apresentar();
 			desafiante.apresentar();
+
+			   int min = 0; // Minimum value of range
+		      int max = 2; // Maximum value of range
+		      // Print the min and max  
+		      System.out.println("Random value in int from "+ min + " to " + max + ":");
+		      // Generate random int value from min to max
+		      int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
+		      // Printing the generated random numbers
+		      System.out.println(random_int);	
 		
 			
 			
--- wrk_estudos/Aula5-ObjetosCompostos/src/Main.java
+++ wrk_estudos/Aula5-ObjetosCompostos/src/Main.java
@@ -6,6 +6,15 @@ public class Main {
 		Lutador l1 = new Lutador("Leozin", "Brazil", 17, 1.70, 71.0, 10, 5, 1);
 		l1.apresentar();
 
+			   int min = 0; // Minimum value of range
+		      int max = 2; // Maximum value of range
+		      // Print the min and max  
+		      System.out.println("Random value in int from "+ min + " to " + max + ":");
+		      // Generate random int value from min to max
+		      int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
+		      // Printing the generated random numbers
+		      System.out.println(random_int);	
+
 	}
 
 }
